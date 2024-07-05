4cb71a9f0dcf927ed5d418a111c6ac1cb293cbc9
flavio-franceschetti <flaviofranceschetti98@gmail.com>
2024-07-06 00:24:46 +0200

    feat: header component

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,29 @@ export default {
 </script>
 
 <template>
-  <HeaderApp />
+  <!-- header -->
+  <header class="container">
+    <HeaderApp />
+  </header>
+  <!-- main -->
+  <main class="content-section">
+    <div class="container"></div>
+  </main>
+  <!-- icons section -->
+  <div class="icon-section">
+    <div class="container"></div>
+  </div>
+  <!-- footer -->
+  <footer>
+    <!-- firstfooter -->
+    <div class="first-footer">
+      <div class="container"></div>
+    </div>
+    <!-- second footer -->
+    <div class="second-footer">
+      <div class="container"></div>
+    </div>
+  </footer>
 </template>
 
 <style lang="scss">
--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -5,7 +5,72 @@ export default {
 </script>
 
 <template>
-  <h1>Prova</h1>
+  <div class="container">
+    <!-- logo -->
+    <div class="logo">
+      <img src="/public/img/dc-logo.png" alt="" />
+    </div>
+    <!-- nav -->
+    <div class="nav-bar">
+      <ul>
+        <li>
+          <a href="#">Characters</a>
+        </li>
+        <li>
+          <a href="#">Comics</a>
+        </li>
+        <li>
+          <a href="#">Movies</a>
+        </li>
+        <li>
+          <a href="#">tv</a>
+        </li>
+        <li>
+          <a href="#">Games</a>
+        </li>
+        <li>
+          <a href="#">Characters</a>
+        </li>
+        <li>
+          <a href="#">Comics</a>
+        </li>
+        <li>
+          <a href="#">Movies</a>
+        </li>
+        <li>
+          <a href="#">tv</a>
+        </li>
+        <li>
+          <a href="#">Games</a>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use "/src/styles/partial/mixins" as *;
+@use "/src/styles/partial/variables" as *;
+
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 150px;
+  background-color: #fff;
+  padding: 20px 0;
+}
+
+.nav-bar {
+  ul {
+    list-style: none;
+    display: flex;
+    gap: 15px;
+  }
+
+  li a {
+    text-transform: uppercase;
+    font-weight: bold;
+  }
+}
+</style>
