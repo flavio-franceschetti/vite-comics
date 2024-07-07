829369583ebfc9c2fcb05d54cbd6b3a88ef33c82
flavio-franceschetti <flaviofranceschetti98@gmail.com>
2024-07-07 22:43:14 +0200

    feat: nav active class functionality

--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -1,6 +1,73 @@
 <script>
 export default {
   name: "HeaderApp",
+  data() {
+    return {
+      links: [
+        {
+          text: "Characters",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "Comics",
+          url: "#",
+          current: true,
+        },
+        {
+          text: "Movies",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "TV",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "Games",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "Collectibles",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "Videos",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "Fans",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "News",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "Shop",
+          url: "#",
+          current: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    // metodo per la classe active nella navigazione
+    clickNavActive(indice) {
+      // ciclo tutti i link e al click rendo tutti i valori di current false
+      this.links.forEach((link) => {
+        link.current = false;
+      });
+      // e rendo il valore di current del link cliccato true
+      this.links[indice].current = true;
+    },
+  },
 };
 </script>
 
@@ -8,40 +75,17 @@ export default {
   <div class="container">
     <!-- logo -->
     <div class="logo">
-      <img src="/public/img/dc-logo.png" alt="" />
+      <img src="/public/img/dc-logo.png" alt="dc logo" />
     </div>
     <!-- nav -->
     <div class="nav-bar">
       <ul>
-        <li>
-          <a href="#">Characters</a>
-        </li>
-        <li>
-          <a class="active" href="#">Comics</a>
-        </li>
-        <li>
-          <a href="#">Movies</a>
-        </li>
-        <li>
-          <a href="#">tv</a>
-        </li>
-        <li>
-          <a href="#">Games</a>
-        </li>
-        <li>
-          <a href="#">Characters</a>
-        </li>
-        <li>
-          <a href="#">Comics</a>
-        </li>
-        <li>
-          <a href="#">Movies</a>
-        </li>
-        <li>
-          <a href="#">tv</a>
-        </li>
-        <li>
-          <a href="#">Games</a>
+        <li
+          @click="clickNavActive(index)"
+          v-for="(link, index) in links"
+          :key="index"
+        >
+          <a :class="link.current ? 'active' : ''" href="#">{{ link.text }}</a>
         </li>
       </ul>
     </div>
